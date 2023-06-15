651106530779b428405f97542793298cfb14a573
StartDeveloperKim <ktw550111@naver.com>
2023-06-15 22:10:27 +0900

    다시 설계

--- study/src/main/java/jwt/study/exception/DuplicateUserException.java
+++
@@ -1,7 +0,0 @@
-package jwt.study.exception;
-
-public class DuplicateUserException extends  RuntimeException{
-    public DuplicateUserException(String message) {
-        super(message);
-    }
-}
--- study/src/main/java/jwt/study/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package jwt.study.repository;
-
-import jwt.study.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
